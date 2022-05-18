edc7f6167e0216168ea887df3585e3d1aefd183a
Code Heart <codeheart09@gmail.com>
2022-05-18 18:16:54 -0300

    base prediction (0.96 acc)

--- functions.py
+++ functions.py
@@ -1,8 +1,11 @@
 from sklearn.datasets import fetch_openml
 import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
 
 
 def get_dataset():
+    print('*** Downloading dataset...')
     return fetch_openml('mnist_784', version=1)
 
 
@@ -17,3 +20,16 @@ def convert_label_type(labels):
 def split_train_test(features, labels):
     return features.iloc[:60000], features.iloc[60000:], labels[:60000], labels[60000:]
 
+
+def train_model(features, labels):
+    print('*** Training model...')
+    model = KNeighborsClassifier()
+    model.fit(features, labels)
+    return model
+
+
+def evaluate_on_test_set(model, features, labels):
+    print('*** Evaluating on test set...')
+    predictions = model.predict(features)
+    accuracy = accuracy_score(labels, predictions)
+    print(f'Model accuracy: {accuracy}')
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from functions import get_dataset, separate_features_labels, convert_label_type, split_train_test
+from functions import get_dataset, separate_features_labels, convert_label_type, split_train_test, \
+    train_model, evaluate_on_test_set
 
 # DATA
 dataset = get_dataset()
 x, y = separate_features_labels(dataset)
 y = convert_label_type(y)
 x_train, x_test, y_train, y_test = split_train_test(x, y)
+model = train_model(x_train, y_train)
+evaluate_on_test_set(model, x_test, y_test)
